8661f1554fc4eebbba629c052eea828d86c8e603
musfiqurcse <md.musfiqurcse@outlook.com>
2017-08-25 16:47:57 +0600

    Tensor flow inline array

--- first_python.py
+++ first_python.py
@@ -1,4 +1,11 @@
 import tensorflow as tf
 hello =tf.constant('Hellow, TensorFlow!')
+a =tf.constant([2,3,4,5,6])
+b =tf.constant([3,4,5,6,7])
+c= tf.add(a,b)
+
 sess=tf.Session()
-print(sess.run(hello))
+with tf.Session() as session:
+    result =session.run(c)
+    print(result)
+# print(sess.run(hello))
